4949d5e1e95032c566c7922581da37f7392cbfea
hasanbatuhan <103295685+hasanbatuhan@users.noreply.github.com>
2026-05-03 01:16:35 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,88 +1,103 @@
 #include <msp430.h>
-
+#include <stdio.h>
+#include "uart.h"             // Yeni oluÅŸturduÄŸumuz UART baÅŸlÄ±ÄŸÄ±
 #include "LiquidCrystal_I2C.h"
-// Global Deðiþkenler (Diðer fonksiyonlarýn eriþebilmesi için)
-unsigned int ldr_degeri = 0;
-char* itoa(int value, char* str, int base) ;
-unsigned long adc_toplam = 0;
-unsigned int ornek_sayaci = 0;
-unsigned int filtreli_ldr = 0;
+#include "packeting.h"
+#include  "datasaver.h"
+
+// --- Global DeÄŸiÅŸkenler ---
+volatile unsigned int ldr_degeri = 0;
+volatile unsigned long adc_toplam = 0;
+volatile unsigned int ornek_sayaci = 0;
+volatile unsigned int filtreli_ldr = 0;
+ char ldr_buffer[13];
+ volatile unsigned int global_time = 0; // Her ÅŸeyin referans alacaÄŸÄ± zaman deÄŸiÅŸkeni
+ unsigned int second_counter = 0;        // 1 saniyeyi yakalamak iÃ§in yardÄ±mcÄ± sayaÃ§
+
+// Fonksiyon Prototipi
+char* itoa(int value, char* str, int base);
 
 void main(void) {
     WDTCTL = WDTPW + WDTHOLD;
 
-    if (CALBC1_1MHZ == 0xFF) {
-            while(1); // Eðer kalibrasyon verisi silinmiþse burada dur (Güvenlik önlemi)
-        }
-        BCSCTL1 = CALBC1_1MHZ;
-        DCOCTL = CAL_DCO_1MHZ;
+    BCSCTL1 = CALBC1_8MHZ;
+    DCOCTL = CALDCO_8MHZ;
 
-char sayýsaldeger[6] ;
-
-
-    // --- ADC10 Yapýlandýrmasý ---
-    ADC10CTL1 = INCH_0; // Þu an sadece A0 (LDR) seçili
+    // --- ADC10 YapÄ±landÄ±rmasÄ± ---
+    ADC10CTL1 = INCH_0; 
     ADC10CTL0 = SREF_0 + ADC10SHT_2 + ADC10ON + ADC10IE;
     ADC10AE0 |= BIT0;
 
-    // --- Timer_A1 Yapýlandýrmasý (PWM - P2.1) ---
+    // --- Timer_A1 (PWM) ---
     P2DIR |= BIT1; P2SEL |= BIT1;
     TA1CCR0 = 1023;
     TA1CCTL1 = OUTMOD_7;
     TA1CTL = TASSEL_2 + MC_1;
 
-    // lcd
-
-
+    // --- DonanÄ±m BaÅŸlatma ---
     lcd_init();
+    uart_init(); // uart.c iÃ§indeki fonksiyonu Ã§aÄŸÄ±rÄ±r
+    datasaver_init_timer();
+
+
 
     lcd_write_string("I:");
-
-
+    
     __enable_interrupt();
 
-    while(1) {
-        ADC10CTL0 |= ENC + ADC10SC;
-        __bis_SR_register(LPM0_bits + GIE);
-        lcd_set_cursor(0,2);
-         itoa(filtreli_ldr, sayýsaldeger, 10);
-         lcd_write_string(sayýsaldeger);
-         lcd_write_string("    ");
 
-        __delay_cycles(25000);
+
+
+    while(1) {
+        // ADC Ã–rneklemesini BaÅŸlat
+        ADC10CTL0 |= ENC + ADC10SC;
+
+
+        // ADC ISR bizi uyandÄ±rana kadar bekle
+        __bis_SR_register(LPM0_bits + GIE);
+        datasaver_op() ;
+        // --- LCD GÃœNCELLEME ---
+        lcd_set_cursor(0,2);
+        itoa(filtreli_ldr,ldr_buffer, 10);
+        lcd_write_string(ldr_buffer);
+        lcd_write_string("    ");
+
+        // --- UART PAKETLEME VE GÃ–NDERME ---
+        // uart_busy kontrolÃ¼ uart_send_string iÃ§inde yapÄ±lÄ±yor
+
+
+         send_packet((int)filtreli_ldr, 100, 34);
+
+
+         __delay_cycles(160000); // 200ms bekleme
+
     }
 }
 
 // --- ADC10 Kesme Servis Rutini ---
-
 #pragma vector=ADC10_VECTOR
 __interrupt void ADC10_ISR(void) {
-    adc_toplam += ADC10MEM; // Her okumayý biriktir
+    adc_toplam += ADC10MEM;
     ornek_sayaci++;
 
-    if (ornek_sayaci >= 128) { // 64 örnek toplandýðýnda
-        ldr_degeri = adc_toplam /128 ; // 64'e böl (bit kaydýrma)
-
-
-        adc_toplam = 0;                 // Toplamý sýfýrla
-        ornek_sayaci = 0;               // Sayacý sýfýrla
+    if (ornek_sayaci >= 128) {
+        ldr_degeri = adc_toplam / 128;
+        adc_toplam = 0;
+        ornek_sayaci = 0;
         filtreli_ldr = ldr_degeri;
-        // Sadece ortalama hazýr olduðunda iþlemciyi uyandýr
-        __bic_SR_register_on_exit(LPM0_bits);
+        
         TA1CCR1 = 900 - filtreli_ldr;
-      }else {
-          ADC10CTL0 |= ENC +ADC10SC ;
-      }
-
+        
+        __bic_SR_register_on_exit(LPM0_bits); // Ana dÃ¶ngÃ¼yÃ¼ uyandÄ±r
+    } else {
+        ADC10CTL0 |= ENC + ADC10SC; // Bir sonraki Ã¶rnek
+    }
 }
-// Basit ve hafýza dostu itoa implementasyonu
-char* itoa(int value, char* str, int base) {
-    char *rc;
-    char *ptr;
-    char *low;
-    // Sadece 10'luk taban için hýzlý çözüm (LDR için yeterli)
-    if (base != 10) return str;
 
+// --- YardÄ±mcÄ± Fonksiyon: itoa ---
+char* itoa(int value, char* str, int base) {
+    char *rc, *ptr, *low;
+    if (base != 10) return str;
     rc = ptr = str;
     if (value < 0) {
         *ptr++ = '-';
@@ -93,9 +108,7 @@ char* itoa(int value, char* str, int base) {
         *ptr++ = "0123456789"[value % 10];
         value /= 10;
     } while (value);
-
     *ptr-- = '\0';
-    // Stringi ters çevirme
     while (low < ptr) {
         char temp = *low;
         *low++ = *ptr;
